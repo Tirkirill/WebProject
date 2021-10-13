2951d4a2d98e78969c62f89d904423ade16491a0
Tirkirill <sherbakirill@mail.ru>
2021-10-14 01:12:26 +0300

    Первые задания

--- game.js
+++ game.js
@@ -1,1 +1,67 @@
 'use strict';
+
+class Vector {
+    constructor(x=0, y=0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(vector) {
+        if (!(vector instanceof Vector)) {
+            throw new Error("Нужно передать вектор!"); 
+        }
+
+        return new Vector(this.x + vector.x, this.y + vector.y);
+    }
+
+    times(n) {
+        if (typeof n !== "number") {
+            throw new Error("Нужно передать число!");
+        }
+
+        return new Vector(this.x*n, this.y*n);
+    }
+}
+
+class Actor {
+    constructor(pos=new Vector(0,0), size=new Vector(1,1), speed=new Vector(0,0)) {
+        if (!(pos instanceof Vector)) {
+            throw new Error("Нужно передать вектор!");
+        }
+        if (!(size instanceof Vector)) {
+            throw new Error("Нужно передать вектор!");
+        }
+        if (!(speed instanceof Vector)) {
+            throw new Error("Нужно передать вектор!");
+        }
+
+        this.pos = pos;
+        this.size = size;
+        this.speed = speed;
+        this._type= "actor";
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    get left() {
+        return this.pos.x;
+    }
+
+    get right() {
+        return this.pos.x + this.size.x;
+    }
+
+    get bottom() {
+        return this.pos.y + this.size.y;
+    }
+
+    get top() {
+        return this.pos.y;
+    }
+
+    act() {
+
+    }
+}
